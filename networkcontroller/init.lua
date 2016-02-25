5898b6afd5792f5b4b2949fdb86d28a1a31e694d
Max <mail@ne0h.de>
2016-02-25 23:55:38 +0100

    Changed temperature sensor to more accurate LMT86.

--- networkcontroller/init.lua
+++ networkcontroller/init.lua
@@ -2,10 +2,23 @@ wifi.setmode(wifi.STATION)
 wifi.sta.config("ssid", "password")
 print(wifi.sta.getip())
 
+function getVoltage()
+    return adc.read(0)*3300/1024
+end
+
+function getTemperatur(input)
+    return 30 + (10.888 - math.sqrt(118.5485 + (0.01388 * (1777.3 - getVoltage())))) / -0.00694
+end
+
 srv=net.createServer(net.TCP) 
 srv:listen(80, function(conn) 
-	conn:on("receive", function(conn, request) 
-	conn:send((adc.read(0)*3300/1024-500)/10)
+	conn:on("receive", function(conn, request)
+    conn:send("HTTP/1.1 200 OK\r\n");
+    conn:send("Content-type: text/json\r\n");
+    conn:send("Connection: close\r\n\r\n");
+	conn:send("{\"node\":\"cabinet\",\"temperature\":")
+    conn:send(getTemperatur())
+    conn:send("}")
 	conn:close()
 	collectgarbage()
 	end) 
