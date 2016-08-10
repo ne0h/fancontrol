05257ec6e8713f7ce5da68eb7301fa44d572e3df
Max <mail@ne0h.de>
2016-08-10 18:32:40 +0200

    avr code updated

--- fancontroller/main.c
+++ fancontroller/main.c
@@ -2,18 +2,13 @@
 
 int main() {
 
-	// Set timer to 8 bit fast pwm mode
-	TCCR0A = (2 << COM0A0) | (2 << COM0B0) | (3 << WGM00);
-	TCCR0B = (0 << WGM02) | (1 << CS00);
+	TCCR0A |= (2 << COM0A0) | (3 << COM0B0) | (3 << WGM00);
+	TCCR0B |= (1 << CS00);
 
-	// Set prescaler to 1
-	TCCR1 = (0 << PWM1A) | (0 << COM1A0) | (1 << CS10);
+	DDRB |= (1 << PB1) | (1 << PB0);
 
-	GTCCR = (1 << PWM1B) | (2 << COM1B0);
-	DDRB  |= (1 << PB1) | (1 << PB0);
-
-	OCR0A = 127;
-	OCR0B = 255;
+	OCR0A = 50;
+	OCR0B = 50;
 
 	while (1) {
 
